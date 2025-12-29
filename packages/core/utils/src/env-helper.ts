9e02ecb926bcaaeee6fbf3d2f38df612aa648094
Andrei L <1881266+unrevised6419@users.noreply.github.com>
2025-12-29 22:50:00 +0200

    fix: move functions outside utils object

--- packages/core/utils/src/env-helper.ts
+++ packages/core/utils/src/env-helper.ts
@@ -10,90 +10,98 @@ function getKey(key: string) {
   return process.env[key] ?? '';
 }
 
+function intFn(key: string, defaultValue?: number): number | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return parseInt(getKey(key), 10);
+}
+
+function floatFn(key: string, defaultValue?: number): number | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return parseFloat(getKey(key));
+}
+
+function boolFn(key: string, defaultValue?: boolean): boolean | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return getKey(key) === 'true';
+}
+
+function jsonFn(key: string, defaultValue?: object) {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  try {
+    return JSON.parse(getKey(key));
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Invalid json environment variable ${key}: ${error.message}`);
+    }
+
+    throw error;
+  }
+}
+
+function arrayFn(key: string, defaultValue?: string[]): string[] | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  let value = getKey(key);
+
+  if (value.startsWith('[') && value.endsWith(']')) {
+    value = value.substring(1, value.length - 1);
+  }
+
+  return value.split(',').map((v) => {
+    return _.trim(_.trim(v, ' '), '"');
+  });
+}
+
+function dateFn(key: string, defaultValue?: Date): Date | undefined {
+  if (!_.has(process.env, key)) {
+    return defaultValue;
+  }
+
+  return new Date(getKey(key));
+}
+
+/**
+ * Gets a value from env that matches oneOf provided values
+ * @param {string} key
+ * @param {string[]} expectedValues
+ * @param {string|undefined} defaultValue
+ * @returns {string|undefined}
+ */
+function oneOfFn(key: string, expectedValues?: unknown[], defaultValue?: unknown) {
+  if (!expectedValues) {
+    throw new Error(`env.oneOf requires expectedValues`);
+  }
+
+  if (defaultValue && !expectedValues.includes(defaultValue)) {
+    throw new Error(`env.oneOf requires defaultValue to be included in expectedValues`);
+  }
+
+  const rawValue = env(key, defaultValue);
+  return expectedValues.includes(rawValue) ? rawValue : defaultValue;
+}
+
 const utils = {
-  int(key: string, defaultValue?: number): number | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return parseInt(getKey(key), 10);
-  },
-
-  float(key: string, defaultValue?: number): number | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return parseFloat(getKey(key));
-  },
-
-  bool(key: string, defaultValue?: boolean): boolean | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return getKey(key) === 'true';
-  },
-
-  json(key: string, defaultValue?: object) {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    try {
-      return JSON.parse(getKey(key));
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Invalid json environment variable ${key}: ${error.message}`);
-      }
-
-      throw error;
-    }
-  },
-
-  array(key: string, defaultValue?: string[]): string[] | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    let value = getKey(key);
-
-    if (value.startsWith('[') && value.endsWith(']')) {
-      value = value.substring(1, value.length - 1);
-    }
-
-    return value.split(',').map((v) => {
-      return _.trim(_.trim(v, ' '), '"');
-    });
-  },
-
-  date(key: string, defaultValue?: Date): Date | undefined {
-    if (!_.has(process.env, key)) {
-      return defaultValue;
-    }
-
-    return new Date(getKey(key));
-  },
-
-  /**
-   * Gets a value from env that matches oneOf provided values
-   * @param {string} key
-   * @param {string[]} expectedValues
-   * @param {string|undefined} defaultValue
-   * @returns {string|undefined}
-   */
-  oneOf(key: string, expectedValues?: unknown[], defaultValue?: unknown) {
-    if (!expectedValues) {
-      throw new Error(`env.oneOf requires expectedValues`);
-    }
-
-    if (defaultValue && !expectedValues.includes(defaultValue)) {
-      throw new Error(`env.oneOf requires defaultValue to be included in expectedValues`);
-    }
-
-    const rawValue = env(key, defaultValue);
-    return expectedValues.includes(rawValue) ? rawValue : defaultValue;
-  },
+  int: intFn,
+  float: floatFn,
+  bool: boolFn,
+  json: jsonFn,
+  array: arrayFn,
+  date: dateFn,
+  oneOf: oneOfFn,
 };
 
 const env: Env = Object.assign(envFn, utils);
